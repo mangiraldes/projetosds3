b685ad89f81cc65facdbbce2d803741cde06e4c2
manoel giraldes <manogiraldes@gmail.com>
2021-09-11 18:07:47 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
 import NavBar from "components/navbar";
 import Footer from "components/footer";
 import DataTable from "components/DataTable";
+import Barchart from "components/BarChart"
+import DonutChart from 'components/Donut'
 
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className="container">
+        <div className="row px-3">
+
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <Barchart />
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <DonutChart />
+        </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
@@ -17,4 +33,3 @@ function App() {
 }
 
 export default App;
-
